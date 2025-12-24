aa01dfc9bb355484216353e1b1371c3acd018ec2
Oleksandr <1eksandr999@gmail.com>
2025-12-24 21:12:45 +0200

    prepare for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-};
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'dice-game';
 
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  },
+};
 export default nextConfig;
